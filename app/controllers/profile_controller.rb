f01e60504f7586475a06c6599d6dd2c88da998eb
John Poulin <john.m.poulin@gmail.com>
2022-10-02 22:12:03 -0400

    Added some layout with devise

---
+++ app/controllers/profile_controller.rb
@@ -0,0 +1,12 @@
+class ProfileController < ApplicationController
+    before_action :authenticate_user!
+
+    def show
+    end
+
+    def edit
+    end
+    
+    def update
+    end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  devise_for :users, controllers: {
+    sessions: 'users/sessions'
+  }
+  
   root to: "home#index"
 
   namespace :admin do
