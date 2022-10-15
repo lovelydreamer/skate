55fd58a8c1e1f6c7fb1b0a602c4761e8b73c71f5
John Poulin <john.m.poulin@gmail.com>
2022-10-14 22:29:54 -0400

    Added SecureHeaders

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
     # TODO: implement hook to ensure that state-changing requests are audited
+    after_action :require_audit_trail
+
+    private
+    def require_audit_trail
+        # For non-get requests, ensure that requests are audited
+        
+    end
 end
--- config/environments/development.rb
+++ config/environments/development.rb
@@ -73,4 +73,33 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
+
+  SecureHeaders::Configuration.default do |config|
+    # Add "; preload" and submit the site to hstspreload.org for best protection.
+    config.x_frame_options = "DENY"
+    config.x_content_type_options = "nosniff"
+    config.x_xss_protection = "1; mode=block"
+    config.x_download_options = "noopen"
+    config.x_permitted_cross_domain_policies = "none"
+    config.referrer_policy = %w(origin-when-cross-origin strict-origin-when-cross-origin)
+    config.csp = {
+      # "meta" values. these will shape the header, but the values are not included in the header.
+      preserve_schemes: true, # default: false. Schemes are removed from host sources to save bytes and discourage mixed content.
+      disable_nonce_backwards_compatibility: true, # default: false. If false, `unsafe-inline` will be added automatically when using nonces. If true, it won't. See #403 for why you'd want this.
+  
+      # directive values: these values will directly translate into source directives
+      default_src: %w('self'),
+      base_uri: %w('self'),
+      block_all_mixed_content: true, # see https://www.w3.org/TR/mixed-content/
+      child_src: %w('self'), # if child-src isn't supported, the value for frame-src will be set.
+      font_src: %w(https://cdn.jsdelivr.net 'self' data:),
+      form_action: %w('self' github.com),
+      frame_ancestors: %w('none'),
+      img_src: %w('self'),
+      manifest_src: %w('self'),
+      object_src: %w('self'),
+      script_src: %w(https://unpkg.com https://cdn.jsdelivr.net 'self'),
+      style_src: %w(https://unpkg.com https://cdn.jsdelivr.net 'self' 'unsafe-inline'),
+    }
+  end
 end
