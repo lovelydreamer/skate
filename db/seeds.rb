350c965ad45f5f8bddebd3974de8b9ca317cded3
John Poulin <john.m.poulin@gmail.com>
2023-03-14 09:44:25 -0400

    Fixing some bugs

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,6 +1,7 @@
 category_trucks = ProductCategory.create(name: "Trucks", description: "The item that connects the wheels to the deck")
 category_decks = ProductCategory.create(name: "Decks", description: "The item that you put your feet on")
 
+<<<<<<< Updated upstream
 element_deck = Product.create(
     name: "Element board", 
     description: "The one that Bam rides",
@@ -24,3 +25,28 @@ tensor_trucks = Product.create(
     cost: 18.75,
     product_category: category_trucks
 )
+=======
+Product.create(
+  name: 'Element board',
+  description: 'The one that Bam rides',
+  price: 59.99,
+  cost: 32.75,
+  product_category: category_decks
+)
+
+Product.create(
+  name: 'Blind board',
+  description: 'Reaper',
+  price: 53.99,
+  cost: 35.75,
+  product_category: category_decks
+)
+
+Product.create(
+  name: 'Tensor Trucks',
+  description: 'These things rip',
+  price: 32.99,
+  cost: 18.75,
+  product_category: category_trucks
+)
+>>>>>>> Stashed changes
