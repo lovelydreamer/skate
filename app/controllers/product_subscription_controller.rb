553c9c3f11812798b4c5555a0a83bb775e02fa74
John Poulin <john.m.poulin@gmail.com>
2023-04-06 00:59:27 +0000

    Setting the stage

---
+++ app/controllers/product_subscription_controller.rb
@@ -0,0 +1,13 @@
+#
+# Our customers have been interested in product notifications. 
+# This endpoint serves as a subscription-type model so customers
+# can subscribe for updates to products.
+#
+class ProductSubscriptionController < ApplicationController
+    def index
+
+    end
+
+    def show
+    end
+end
---
+++ app/models/product_subscription.rb
@@ -0,0 +1,23 @@
+require 'uri'
+require 'net/http'
+
+class ProductSubscription < ApplicationRecord
+    belongs_to :product
+    belongs_to :user
+
+    def notify
+        unless valid_url?
+            raise "InvalidURL"
+        end
+
+        uri = URI(self.url)
+        res = Net::HTTP.post_form(uri, 'product_id' => self.product_id)
+
+        return res.body
+    end
+
+    private
+    def valid_url?
+        true
+    end
+end
