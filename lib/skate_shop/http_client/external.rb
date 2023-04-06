475eaea4ca304f28faf3dd71723587e5056769ee
John Poulin <john.m.poulin@gmail.com>
2023-04-06 20:15:19 +0000

    External request library has proxy

--- lib/skate_shop/http_client/external.rb
+++ lib/skate_shop/http_client/external.rb
@@ -5,8 +5,18 @@ require 'net/http'
 module SkateShop
   module HttpClient
     class External < Net::HTTP
-      def post(url, options = nil)
-        super.post(url, options)
+      def self.post(url, data, options = nil)
+        self.validate_url!(url)
+
+        uri = URI.parse(url)
+        http = Net::HTTP.new(uri.host, nil, "196.189.44.49", "1234")
+
+        res = http.post(uri, data, options)
+      end
+
+      private
+      def self.validate_url!(url)
+        raise "InvalidProtocol" unless url =~ /^https/
       end
     end
   end
