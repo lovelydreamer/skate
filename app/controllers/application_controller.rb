987fa2fedde8971d07dfbe1cf7155059cbfc0131
John Poulin <john.m.poulin@gmail.com>
2023-04-07 18:31:41 +0000

    Added some DiD measures for admin

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,6 +5,7 @@
 # TODO: Hook to connect as read-only to database if there's no state-changing request
 class ApplicationController < ActionController::Base
     include ApplicationHelper
+    #before_action :check_controller
 
     # TODO: This should be re-named, since we're technically not _requiring_ anything
     #after_action :require_audit_trail
@@ -19,5 +20,15 @@ class ApplicationController < ActionController::Base
     # TODO: Maybe this should be tied to request_id instead?
     def audited?
         Rails.cache.read('audited', raw: true)
+    end
+
+    def check_controller
+        
+        if request.env['PATH_INFO'].match(/\A\/admin/)
+            # If the user has not hit the admin controller there's a gap in our inheritance logic
+            unless Rails.cache.read('admin_request_processed', raw: true)
+                raise "Admin Request didn't flow through admin controllers"
+            end
+        end
     end
 end
