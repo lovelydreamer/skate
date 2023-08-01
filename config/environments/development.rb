32e065dc55d62fb93c7144c13b93c65cb089e64c
John Poulin <john.m.poulin@gmail.com>
2023-08-01 09:55:46 -0400

    Codespaces URLs updated

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -3,7 +3,7 @@ require "active_support/core_ext/integer/time"
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
-  config.hosts << /.*\.preview\.app\.github\.dev/ # To support codespaces, open this up wideeeee
+  config.hosts << /.*\.github\.dev/ # To support codespaces, open this up wideeeee
 
   # In the development environment your application's code is reloaded any time
   # it changes. This slows down response time but is perfect for development
